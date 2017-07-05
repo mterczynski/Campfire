330f044e60d2630ce87781be8ba24ea6aec6c762
Michał Terczyński <mterczynski271@gmail.com>
2017-07-05 15:00:41 +0200

    First usable version

--- Campfire.js
+++ Campfire.js
@@ -1,13 +1,17 @@
-class Fire
+class Campfire
 {
-    constructor()
+    constructor(parametrsObject = {numberOfParticles:300, particleSpeed:1.2, width:23, fireHeight:25 })
     {
         this.object3d = new THREE.Object3D();
+        this.width = parametrsObject.width || 23;
+        this.numberOfParticles = parametrsObject.numberOfParticles || 300;
+        this.particleSpeed = parametrsObject.particleSpeed || 1.2;
+        this.fireHeight = parametrsObject.fireHeight || 25;
     }
-    getObject3d()
+    getNewObject3d()
     {
         const particleSize = [3,3,3];
-        const fireHeight = 25;
+        const fireHeight = this.fireHeight;
         const that = this; // reference to main object
         const particleMaterial = new THREE.MeshBasicMaterial
         ({
@@ -19,9 +23,9 @@ class Fire
         });
         const generate = function()
         {
-            const quantity = 300
+            const quantity = that.numberOfParticles;
             const particles = scene.children.filter((el)=>{return el.name == "particle"});
-            const maxSpread = 23;
+            const maxSpread = that.width;
             particles.map((el)=>{that.object3d.remove(el)});
             for(let i=0; i<quantity; i++)
             {
@@ -40,29 +44,24 @@ class Fire
         
         light.position.set(0,10,0);
         this.object3d.add(light);
-        this.object3d.userData.updateParticles = function()
-        {
-            const speedBuffer = 1.2;
-            const particles = that.object3d.children.filter((el)=>{return el.name == "particle"});
-            for(let i=0; i<particles.length; i++)
-            {
-                let particle = particles[i];
-
-                particle.position.y += Math.random() * 0.5 * speedBuffer;
-                particle.material.opacity -= 0.005 * speedBuffer;
-                particle.material.opacity -= particle.userData.radius * 0.001 * speedBuffer;
-                if(particle.position.y >= fireHeight)
-                {
-                    particle.position.y = 0;
-                    particle.material.opacity = 1;
-                }
-            }
-        }
         generate();
         return this.object3d;
     }
     updateParticles()
     {
-        
+        let particles = this.object3d.children.filter((el)=>{return el.name == "particle"});
+        for(let i=0; i<particles.length; i++)
+        {
+            let particle = particles[i];
+
+            particle.position.y += Math.random() * 0.5 * this.particleSpeed;
+            particle.material.opacity -= 0.005 * this.particleSpeed;
+            particle.material.opacity -= particle.userData.radius * 0.001 * this.particleSpeed;
+            if(particle.position.y >= this.fireHeight)
+            {
+                particle.position.y = 0;
+                particle.material.opacity = 1;
+            }
+        }
     }
 }
